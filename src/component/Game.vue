53217271da32ef1df82c25f58520759388832fbf
DIO0550 <>
2019-11-17 01:24:45 +0900

    ボムの数を数える処理を追加。ボムの位置をMapで保持するように変更。

--- src/component/Game.vue
+++ src/component/Game.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <template v-for="i in $data.number_of_column">
-            <Panel v-for="j in $data.number_of_row" v-bind:key="100 * i + j" :row="j" :column="i" :bomb="is_bomb(j, i)"/>
+        <template v-for="i in number_of_row">
+            <Panel v-for="j in number_of_column" v-bind:key="number_of_column * (i - 1) + (j - 1)" 
+                                :row="(i - 1)" :column="(j - 1)" :bomb="is_bomb((i - 1), (j - 1))"
+                                :around_bomb_num="around_bomb_num((i - 1), (j - 1))"/>
         </template>
     </div>
 </template>
@@ -18,17 +20,20 @@ export default {
         return {
             number_of_row:PanelConst.high_lebel.row,
             number_of_column:PanelConst.high_lebel.column,
-            bombs:[]
+            bombs:new Map()
         }
     },
     components: {
         Panel
     },
     methods: {
+        /**
+         * ボムを生成する
+         */
         create_bombs() {
             var panel_list = [];
             var panel_num = PanelConst.high_lebel.row * PanelConst.high_lebel.column;
-            for (var i = 0;  i < panel_num;  i++) {
+            for (var i = 0;  i < panel_num; i++) {
                 panel_list.push(i)
             }
 
@@ -49,31 +54,123 @@ export default {
                 var row = Math.floor(number /  PanelConst.high_lebel.column);
                 console.log("column=" + column);
                 console.log("row =" + row);
-
-                this.$data.bombs.push(
-                    {
-                        column: column,
-                        row: row
-                    }
-                );
+                this.bombs.set(number, {
+                    column: column,
+                    row: row
+                });
             }
-
         },
+        /**
+         * 引数で受け取ったパネルがボム持ちか返す
+         * @param row 行
+         * @param column 列
+         * @returns ボムかどうか 
+         */
         is_bomb(row, column) {
             console.log("START is_bomb")
-            for (var i = 0; i < this.$data.bombs.length; i++) {
-                var bomb = this.$data.bombs[i];
-                if (bomb.row == row && bomb.column == column) {
-                    console.log("END is_bomb true")
-                    return true;
+            var has_bomb = this.bombs.has(column + row * this.number_of_column)
+            console.log("END is_bomb false")
+            return has_bomb;
+        },
+        /**
+         * 周りのボムの数を返す
+         * @param row 行
+         * @param column 列
+         * @returns ボムの数 
+         */
+        around_bomb_num(row, column) {
+            console.log("START around_bomb_num");
+            var bomb_num = this.top_bomb_num(row, column);
+            bomb_num += this.side_bomb_num(row, column);
+            bomb_num += this.bottom_bomb_num(row, column);
+            console.log("END around_bomb_num bomb_num =" + bomb_num);
+            return bomb_num;
+        },
+        /**
+         * 上下のボムの数を返す
+         * @param row 行
+         * @param column 列
+         * @returns ボムの数 
+         */
+        top_bomb_num(row, column) {
+            console.log("START around_bomb_num");
+            var bomb_num = 0;
+            for (var i = 0; i < 3; i++) {
+                var check_row = row - 1;
+                var check_column = column + (i - 1);
+                // 範囲外
+                if (this.is_out_of_bounds(check_row, check_column)) {
+                    continue;
+                }
+                if (this.is_bomb(check_row, check_column)) {
+                    bomb_num++;
                 }
             }
-            console.log("END is_bomb false")
+            console.log("END around_bomb_num");
+            return bomb_num;
+        },
+        /**
+         * 左右のボムの数を返す
+         * @param row 行
+         * @param column 列
+         * @returns ボムの数 
+         */
+        side_bomb_num(row, column) {
+            console.log("START side_bomb_num");
+            var bomb_num = 0;
+            for (var i = 0; i < 2; i++) {
+                var check_column = column + ((i * 2) - 1);
+                // 範囲外
+                if (this.is_out_of_bounds(row, check_column)) {
+                    continue;
+                }
+                if (this.is_bomb(row, check_column)) {
+                    bomb_num++;
+                }
+            }
+            console.log("END side_bomb_num");
+            return bomb_num;
+        },
+        bottom_bomb_num(row, column) {
+            console.log("START bottom_bomb_num");
+            var bomb_num = 0;
+            for (var i = 0; i < 3; i++) {
+                var check_row = row + 1;
+                var check_column = column + (i - 1);
+                // 範囲外
+                if (this.is_out_of_bounds(check_row, check_column)) {
+                    continue;
+                }
+                if (this.is_bomb(check_row, check_column)) {
+                    bomb_num++;
+                }
+            }
+            console.log("END bottom_bomb_num");
+            return bomb_num;
+        },
+        /**
+         * 引数で受け取った行と列が範囲外か返す
+         * @param row 行
+         * @param column 列
+         * @return 範囲外か
+         */
+        is_out_of_bounds(row, column) {
+            console.log("START is_out_of_bounds");
+            if (row < 0 || column < 0) {
+                console.log("END is_out_of_bounds return = true");
+                return true;
+            }
+
+            if (row > (this.number_of_row - 1) || column > (this.number_of_column - 1)) {
+                console.log("END is_out_of_bounds return = true");
+                return true;
+            }
+            console.log("END is_out_of_bounds return = false");
             return false;
         }
-
     },
     created() {
+        // ボム生成
         this.create_bombs()
     }
 }
