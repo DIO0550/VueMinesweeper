4ddc9bdefc86610fa04327f130e7a0cfcdabc7ca
DIO0550 <diot12345@yahoo.co.jp>
2019-11-24 18:32:57 +0900

    計算系を行うUtilファイル作成

---
+++ src/javascript/Util/calc_util.js
@@ -0,0 +1,25 @@
+
+/**
+ * 計算系
+ */
+var calc_util = {
+    number_place(target, place) {
+        if (place == 1) {
+            return target % 10;
+        }
+        
+        var length = String(target).length;
+        var divide_value = Math.pow(10, place)
+
+        if (length == place) {
+            return Math.floor(target / (divide_value / 10));
+        }
+
+        var result = target % divide_value;
+        result = Math.floor(result / (divide_value / 10));
+
+        return result;
+    }
+};
+
+export default calc_util;
